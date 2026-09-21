917ac5a7e2dc340d02c2bb13a4300c3d80055f6f
Daniel Spezialetti <61531288+djspezialetti@users.noreply.github.com>
2026-09-21 18:00:40 -0400

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { requireAuth, requireAdmin, resetPassword } = require('../auth');
+const { requireAuth, requireAdmin, resetPassword } = require('../auth-service');
 const services = require('../services');
 
 const router = express.Router();
