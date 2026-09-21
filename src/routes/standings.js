d5160249b8c736b24c3a7a00bc830925d7e45bbc
Daniel Spezialetti <61531288+djspezialetti@users.noreply.github.com>
2026-09-21 18:01:32 -0400

    Update requireAuth import path in standings.js

--- src/routes/standings.js
+++ src/routes/standings.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { requireAuth } = require('../auth');
+const { requireAuth } = require('../auth-service');
 const services = require('../services');
 
 const router = express.Router();
